9ba100f704b2d6e2047932dd28c61ca8f04fb848
Brad Lee <brad903@naver.com>
2019-01-25 14:01:48 +0900

    프로필 사진 파일 에러 발생시 처리 구현

--- src/main/java/codesquad/web/ImageController.java
+++ src/main/java/codesquad/web/ImageController.java
@@ -3,6 +3,7 @@ package codesquad.web;
 import codesquad.service.FileService;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -24,11 +22,20 @@ public class ImageController {
     @Resource(name = "fileService")
     private FileService fileService;
 
+    @Value("${default.image.path}")
+    private String defaultImagePath;
+
     @GetMapping(value = "/image/{fileName}", produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE})
     public @ResponseBody byte[] getImage(@PathVariable String fileName) throws IOException {
         File image = fileService.serveFile(fileName);
         log.debug("image name : {}", image.getName());
-        InputStream in = new FileInputStream(image);
+        InputStream in;
+        try {
+            in = new FileInputStream(image);
+        } catch (FileNotFoundException e) {
+            log.error("image not found!!");
+            in = new FileInputStream(defaultImagePath);
+        }
         return IOUtils.toByteArray(in);
     }
 }
